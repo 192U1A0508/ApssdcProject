d068844211b45b52d22e743764dd019c36086a84
janu <janu@gmail.com>
2021-07-16 18:16:53 +0530

    done

--- StoryProject/app/src/main/java/com/example/storyproject/FolkActivity5.java
+++ StoryProject/app/src/main/java/com/example/storyproject/FolkActivity5.java
@@ -3,12 +3,70 @@ package com.example.storyproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class FolkActivity5 extends AppCompatActivity {
+    Button playPause,stop;
+    TextView cnt;
+    TextToSpeech t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folk5);
+        playPause=findViewById(R.id.play);
+        cnt=findViewById(R.id.mancontent);
+        stop=findViewById(R.id.stop);
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+
+                if (status!=TextToSpeech.ERROR){
+                    t1.setLanguage(Locale.ENGLISH);
+                }
+
+
+
+
+            }
+
+        });
+        playPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=cnt.getText().toString();
+                t1.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+                Toast.makeText(FolkActivity5.this,"playing...",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+        if (t1!=null){
+            t1.stop();
+            t1.shutdown();
+        }
+
+    }
+
+    public void stop(View view) {
+        if (t1 != null) {
+            t1.stop();
+            Toast.makeText(FolkActivity5.this, "Stopped...", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
--- StoryProject/app/src/main/java/com/example/storyproject/HomeActivity.java
+++ StoryProject/app/src/main/java/com/example/storyproject/HomeActivity.java
@@ -1,62 +1,77 @@
 package com.example.storyproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.Locale;
 
+import static android.view.View.IMPORTANT_FOR_ACCESSIBILITY_NO;
+
 public class HomeActivity extends AppCompatActivity {
-    ToggleButton playPause;
+    Button playPause,stop;
     TextView cnt;
-    TextToSpeech textToSpeech;
+    TextToSpeech t1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        playPause=findViewById(R.id.play);
-        cnt=findViewById(R.id.content);
-        textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        playPause = findViewById(R.id.play);
+        cnt = findViewById(R.id.content);
+        stop=findViewById(R.id.stop);
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                
-
-                    if (status==textToSpeech.SUCCESS){
-                        int lang=textToSpeech.setLanguage(Locale.ENGLISH);
-                    }
 
 
-                    }
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.ENGLISH);
+                }
 
 
-
-
-
-
+            }
 
         });
         playPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String s=cnt.getText().toString();
-                int speech=textToSpeech.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+                String s = cnt.getText().toString();
+                t1.speak(s, TextToSpeech.QUEUE_FLUSH, null);
+                Toast.makeText(HomeActivity.this, "Playing...", Toast.LENGTH_SHORT).show();
             }
+
         });
+    }
+    @Override
+    public void onDestroy() {
 
+        super.onDestroy();
+        if (t1!=null){
+            t1.shutdown();
+        }
+    }
 
+    public void stop(View view) {
+        if(t1!=null){
+            t1.stop();
+            Toast.makeText(HomeActivity.this, "Stopped...", Toast.LENGTH_SHORT).show();
+
+        }
 
     }
 
-
-    }
+}
 
